3bb7dae3988ce0d2eafc3e46229013c9ad2d7793
Kenton Lam <kenton_lam@outlook.com>
2018-08-21 18:35:38 +1000

    Change main to create json files from html.

--- main.py
+++ main.py
@@ -12,40 +12,14 @@ from UQCourses.Program import Program
 
 import json
 from LoadCourses.Encoder import CourseJSONEncoder
-programs = UQCourseScraper.create_all_programs("https://my.uq.edu.au/programs-courses/browse.html?level=ugpg", "2018-1")
+json_encoder = json.encoder.JSONEncoder(indent=4)
+_json = json_encoder.encode
+with open('full_ug_programs.html') as ug_html:
+    with open('uq_programs.json', 'w') as programs_file:
+        programs_file.write(_json(list(UQCourseScraper.get_programs(ug_html))))
+    
+    ug_html.seek(0)
+
+    with open('uq_plans.json', 'w') as plans_file:
+        plans_file.write(_json(list(UQCourseScraper.get_plans(ug_html))))
 print(programs)
-
-
-# scraper = UQCourseScaper()
-# scraper.programs["Bachelor of Engineering (Honours)"] = program
-# scraper.serialise("engg-2018")
-# course = Course("span1000", "spanish1", 2, "https://my.uq.edu.au/programs-courses/course.html?course_code=ECON1010")
-# print(course)
-exit()
-
-
-
-scraper = UQCourseScraper("https://my.uq.edu.au/programs-courses/browse.html?level=ugpg")
-scraper.find_all_courses()
-scraper.serialise("UQ-course-2018-sem-1")
-
-
-
-
-
-programs = scraper.get_single_programs()
-courses = list(scraper.get_all_course(programs[0]))
-
-for course in courses:
-    print(course, course.get_extended_info().keys())
-
-# print(scraper.get_all_course(programs[0]))
-# print(scraper.get_all_course("Agribusiness"))
-
-for program in scraper.get_single_programs():
-    print(program, len(scraper.get_all_course(program)))
-
-
-temp = scraper.get_all_course("Advanced Business (Honours)")
-
-
